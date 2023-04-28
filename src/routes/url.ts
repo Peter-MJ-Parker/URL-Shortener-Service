163e6d61592bce15352d6103df2044707ceea59c
D3ord3NidAm <georgeneedham7@gmail.com>
2023-04-28 03:28:24 +0000

    refactor: correct url port

--- src/routes/url.ts
+++ src/routes/url.ts
@@ -1,49 +1,26 @@
 import express from 'express';
 import validUrl from 'valid-url';
 import shortid from 'shortid';
-
-// creating express route handler
-export const urlRouter = express.Router();
-
-// import the Url database model
 import Url from '#schemas/urlModel';
+import { env } from '#config/utils';
 
-// @route    POST /api/url/shorten
-// @description     Create short URL
-
-// The API base Url endpoint
-const baseUrl = 'http:localhost:5000';
-
+const baseUrl = `http://localhost:${env.PORT}`;
+export const urlRouter = express.Router();
 urlRouter.post('/shorten', async (req, res) => {
-	const { longUrl } = req.body; // destructure the longUrl from req.body.longUrl
-
-	// check base url if valid using the validUrl.isUri method
+	const { longUrl } = req.body;
 	if (!validUrl.isUri(baseUrl)) {
 		return res.status(401).json('Invalid base URL');
 	}
-
-	// if valid, we create the url code
 	const urlCode = shortid.generate();
-
-	// check long url if valid using the validUrl.isUri method
 	if (validUrl.isUri(longUrl)) {
 		try {
-			/* The findOne() provides a match to only the subset of the documents 
-            in the collection that match the query. In this case, before creating the short URL,
-            we check if the long URL was in the DB ,else we create it.
-            */
 			let url = await Url.findOne({
 				longUrl,
 			});
-
-			// url exist and return the respose
 			if (url) {
 				res.json(url);
 			} else {
-				// join the generated short code the the base url
 				const shortUrl = baseUrl + '/' + urlCode;
-
-				// invoking the Url model and saving to the DB
 				url = new Url({
 					longUrl,
 					shortUrl,
@@ -54,7 +31,6 @@ urlRouter.post('/shorten', async (req, res) => {
 				res.json(url);
 			}
 		} catch (err) {
-			// exception handler
 			console.log(err);
 			res.status(500).json('Server Error');
 		}
